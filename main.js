2d483580cda11b6a38d7ee006f441ef32636c0c7
BelKed <66956532+BelKed@users.noreply.github.com>
2021-04-05 08:03:34 +0200

    Format files

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
- var MenuItems = document.getElementById("MenuItems");
+var MenuItems = document.getElementById("MenuItems");
 var Mailer = document.getElementById("contact-form");
 var Thanks = document.getElementById("thanks");
-
 
 var name_input = document.getElementById("name");
 var email_input = document.getElementById("email");
@@ -12,41 +11,39 @@ var Scroll = document.getElementById("scroll");
 MenuItems.style.maxHeight = "0px";
 Mailer.style.display = "none";
 Thanks.style.display = "none";
-function control(){
-    if (screen.width>800){
+function control() {
+    if (screen.width > 800) {
         MenuItems.style.maxHeight = "0px";
-        
+
     }
-    if (screen.width>600 && screen.height>750){
+    if (screen.width > 600 && screen.height > 750) {
         Mailer.style.display = "none";
         Thanks.style.display = "none";
     }
 }
-function kill(){
+function kill() {
     MenuItems.style.maxHeight = "0px";
 }
-function menutoggle(){
-    if(MenuItems.style.maxHeight == "0px")
-    {
+function menutoggle() {
+    if (MenuItems.style.maxHeight == "0px") {
         MenuItems.style.maxHeight = "300px";
     }
-    else
-    {
+    else {
         MenuItems.style.maxHeight = "0px";
     }
 }
-function send_mail(){
-        if (Mailer.style.display == "none"){
-            Mailer.style.display = "block";   
-            Scroll.style.overflow = "hidden";
-            Scroll.style.height = "100%";
-        }
+function send_mail() {
+    if (Mailer.style.display == "none") {
+        Mailer.style.display = "block";
+        Scroll.style.overflow = "hidden";
+        Scroll.style.height = "100%";
+    }
 }
-function kill_mail(){
-    if (Mailer.style.display == "block"){
+function kill_mail() {
+    if (Mailer.style.display == "block") {
         Mailer.style.display = "none";
     }
-    if (Thanks.style.display == "block"){
+    if (Thanks.style.display == "block") {
         Thanks.style.display = "none";
     }
     Scroll.style.overflow = "auto";
@@ -58,67 +55,67 @@ function kill_mail(){
     email_input.style.borderColor = "black";
     message_input.style.borderColor = "black";
 }
-function submition_thanks(){
+function submition_thanks() {
     Mailer.style.display = "none";
     Thanks.style.display = "block";
     document.getElementById("thanks-h1").innerHTML = "Thank you for the submition, we are usually replying in 24 hours.";
 
 }
 
-function submition_error(){
+function submition_error() {
     submition_thanks();
     document.getElementById("thanks-h1").innerHTML = "Ooops... Something went wrong, come and try again later.";
 }
 
 
 async function handleSubmit(event) {
-event.preventDefault();
-var data = new FormData(event.target);
+    event.preventDefault();
+    var data = new FormData(event.target);
 
-fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-}).then(response => {
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
 
-    if(name_input.value == ""){
-        name_input.style.borderColor = "red";
-    }else{
-        name_input.style.borderColor = "black";
-    }    
-    if(email_input.value == ""){
-        email_input.style.borderColor = "red";
-    } else{
-        email_input.style.borderColor = "black";
-    }
-    if(message_input.value == ""){
-        message_input.style.borderColor = "red";
-    }else{
-        message_input.style.borderColor = "black";
-    }
-    if(name_input.value != "" && email_input.value != "" && message_input.value != ""){
-        submition_thanks();
-        form.reset()
-    }
-    
-}).catch(error => {
-    submition_error();
-});
+        if (name_input.value == "") {
+            name_input.style.borderColor = "red";
+        } else {
+            name_input.style.borderColor = "black";
+        }
+        if (email_input.value == "") {
+            email_input.style.borderColor = "red";
+        } else {
+            email_input.style.borderColor = "black";
+        }
+        if (message_input.value == "") {
+            message_input.style.borderColor = "red";
+        } else {
+            message_input.style.borderColor = "black";
+        }
+        if (name_input.value != "" && email_input.value != "" && message_input.value != "") {
+            submition_thanks();
+            form.reset()
+        }
+
+    }).catch(error => {
+        submition_error();
+    });
 }
 form.addEventListener("submit", handleSubmit)
 
 document.addEventListener("DOMContentLoaded", function () {
-	new SmoothScroll('a[href*="#"]:not([href="#"])', {
-		// easeInOutExpo
-		speed: 800,
-		speedAsDuration: true,
-		customEasing: function (time) {
-			if (time == 0) return 0;
-			if (time == 1) return 1;
-			if ((time /= 1 / 2) < 1) return 1 / 2 * Math.pow(2, 10 * (time - 1)) + 0;
-			return 1 / 2 * (-Math.pow(2, -10 * --time) + 2) + 0;
-		}
-	});
+    new SmoothScroll('a[href*="#"]:not([href="#"])', {
+        // easeInOutExpo
+        speed: 800,
+        speedAsDuration: true,
+        customEasing: function (time) {
+            if (time == 0) return 0;
+            if (time == 1) return 1;
+            if ((time /= 1 / 2) < 1) return 1 / 2 * Math.pow(2, 10 * (time - 1)) + 0;
+            return 1 / 2 * (-Math.pow(2, -10 * --time) + 2) + 0;
+        }
+    });
 });
