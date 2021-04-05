c2d2bcc2cb0c780ca5083c883822049a3fe8a0cd
BelKed <66956532+BelKed@users.noreply.github.com>
2021-04-05 07:53:48 +0200

    Fancy scrolling

--- main.js
+++ main.js
@@ -109,3 +109,16 @@ fetch(event.target.action, {
 }
 form.addEventListener("submit", handleSubmit)
 
+document.addEventListener("DOMContentLoaded", function () {
+	new SmoothScroll('a[href*="#"]:not([href="#"])', {
+		// easeInOutExpo
+		speed: 800,
+		speedAsDuration: true,
+		customEasing: function (time) {
+			if (time == 0) return 0;
+			if (time == 1) return 1;
+			if ((time /= 1 / 2) < 1) return 1 / 2 * Math.pow(2, 10 * (time - 1)) + 0;
+			return 1 / 2 * (-Math.pow(2, -10 * --time) + 2) + 0;
+		}
+	});
+});
